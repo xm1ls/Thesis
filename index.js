ab15382810bcbf0dbd0df627fd6587152c88be52
xm1ls <xm1ls2000@gmail.com>
2025-02-07 22:08:33 +0200

    add basic implementation

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+const canvasPixels = []
+
+io.on("connection", (socket) => {
+    console.log(socket.id)
+    io.emit("firstConnect", canvasPixels)
+
+    socket.on("drawing", (data) => {
+        canvasPixels.push(data);
+        io.emit("drawing", data)
+    })
+});
+
+httpServer.listen(3000, () => console.log("listening"));
