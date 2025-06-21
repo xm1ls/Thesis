b5b14db6ac0ee83acd1dec8c6f54e28d3684fb6e
xm1ls <xm1ls2000@gmail.com>
2025-06-21 04:09:03 +0300

    api url fix

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client'
 import toast from "react-hot-toast";
 import { lobbyStore } from './lobbyStore';
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://thesis-ehnk.onrender.com:3000";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
